9939e9e3c236cab0c39476dee464f6681acca8e7
dariopmaciel <dariopmaciel@hotmail.com>
2024-12-04 11:14:54 -0300

    continuaçao instalacao firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:app_filme_getx/modules/splash/splash_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
