4cac2c4a5684e53a00a490bf4f539f534d554655
dariopmaciel <dariopmaciel@hotmail.com>
2024-12-04 11:44:18 -0300

    ok até aqui

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:app_filme_getx/modules/splash/splash_module.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // final remoteConfig = FirebaseRemoteConfig.instance;
+  // remoteConfig.fetchAndActivate();
+  //!ou
+  FirebaseRemoteConfig.instance.fetchAndActivate();
   runApp(const MyApp());
 }
 
