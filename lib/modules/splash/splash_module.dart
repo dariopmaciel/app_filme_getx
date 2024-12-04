65e4a6680f888bb3c2665cad0215145651a9ac7e
dariopmaciel <dariopmaciel@hotmail.com>
2024-12-04 15:38:00 -0300

    correção

--- lib/modules/splash/splash_module.dart
+++ lib/modules/splash/splash_module.dart
@@ -3,7 +3,7 @@ import 'package:app_filme_getx/application/modules/module.dart';
 import 'package:app_filme_getx/modules/splash/splash_page.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
-class SplashModule extends Module{
+class SplashModule implements Module{
 
   @override
   List<GetPage> routers=[
