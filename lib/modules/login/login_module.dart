24fefcfcc645cb952a31f6af48ab26300dd25143
dariopmaciel <dariopmaciel@hotmail.com>
2024-12-06 10:04:45 -0300

    add bindings, controller

--- lib/modules/login/login_module.dart
+++ lib/modules/login/login_module.dart
@@ -1,12 +1,15 @@
-
 import 'package:app_filme_getx/application/modules/module.dart';
 import 'package:app_filme_getx/modules/login/login_page.dart';
+import 'package:app_filme_getx/modules/splash/splash_bindings.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
-class LoginModule implements Module{
+class LoginModule implements Module {
   @override
-  List<GetPage> routers=[
-    GetPage(name: '/login', page: () =>  const LoginPage(),),
+  List<GetPage> routers = [
+    GetPage(
+      name: '/login',
+      page: () => const LoginPage(),
+      binding: SplashBindings(), //antes de carregar a pagina sera executado este metodo que esta no bindings
+    ),
   ];
-  
 }
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -3,30 +3,33 @@ import 'package:app_filme_getx/modules/splash/splash_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SplashPage extends StatefulWidget {
-  var controller = Get.find<SplashController>();
+class SplashPage extends StatelessWidget {
+  //*Aqui eu delego o controlle a classe. E busca uma instância já registrada previamente no GetX,
+  //*ele não criará uma nova instância, apenas reutilizará a existente.
+  var controller = Get.put<SplashController>(SplashController());
+
+  //*Aqui eu crio uma nova instancio da classe
+  //*Cada vez que essa linha é executada, uma nova instância será criada, sem qualquer gerenciamento adicional.
+  // var controller = SplashController();
+
   SplashPage({super.key});
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
 
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    //! Para criações a serem criadas depois que meu build for construido
-    WidgetsBinding.instance.addPostFrameCallback(
-      //!ESTA CONSTRUÇÃO
-      (timeStamp) {},
-    );
-    super.initState();
-  }
+  // void initState() {
+  //   //! Para criações a serem criadas depois que o 'init' e o 'build' foi construido (addPostFrameCallback)
+  //   WidgetsBinding.instance.addPostFrameCallback(
+  //     //!ESTA CONSTRUÇÃO
+  //     (timeStamp) {},
+  // );
+  //   super.initState();
+  // }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
